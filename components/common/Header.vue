35af6d894541ec1f45b129be0e54a68aa502b43e
Thanh Vu <csnsjh@gmail.com>
2021-08-24 01:45:54 +0700

    update

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -109,7 +109,6 @@ export default Vue.extend({
     },
     changeThemes() {
       this.$store.commit('common/changeTheme');
-      console.log((this as any).$vuetify.breakpoint);
     },
   },
 });
