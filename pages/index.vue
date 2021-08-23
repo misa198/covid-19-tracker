8e44b6e9cee43a8d3b6b0a1eab709245867e5731
Thanh Vu <csnsjh@gmail.com>
2021-08-24 02:13:41 +0700

    covid table

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,26 @@
     <client-only>
       <CovidChartWrapper />
     </client-only>
+    <div class="mt-8">
+      <HomeContainer>
+        <CovidTable />
+      </HomeContainer>
+    </div>
   </v-container>
 </template>
 <script lang="ts">
 import Vue from 'vue';
 import SummarySheet from '~/components/Home/SummarySheet.vue';
 import CovidChartWrapper from '~/components/Home/CovidChartWrapper.vue';
+import CovidTable from '~/components/Home/CovidTable.vue';
+import HomeContainer from '@/components/Home/HomeContainer.vue';
 
 export default Vue.extend({
   components: {
     SummarySheet,
     CovidChartWrapper,
+    CovidTable,
+    HomeContainer,
   },
 });
 </script>
