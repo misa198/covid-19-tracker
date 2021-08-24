70983048eeee4fa10942aeb59a0d4ddd949eb72f
Thanh Vu <csnsjh@gmail.com>
2021-08-25 02:29:26 +0700

    update

--- dist-server/constants/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appApiUrl = exports.covidVnExpressApiUrl = exports.covidVnExpressNewsApiUrl = exports.kompaApiUrl = void 0;
-exports.kompaApiUrl = 'https://corona-api.kompa.ai/graphql';
-exports.covidVnExpressNewsApiUrl = 'https://gw.vnexpress.net/ar/get_rule_2?category_id=1001005&limit=100&page=1&data_select=article_id,article_type,privacy,title,lead,share_url,thumbnail_url,new_privacy,publish_time,original_cate,off_thumb,iscomment&exclude_id=4332910';
-exports.covidVnExpressApiUrl = 'https://vnexpress.net/microservice/sheet/type/covid19_2021_by_day';
-exports.appApiUrl = 'http://localhost:5000/api';
