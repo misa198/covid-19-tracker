401cf6b3dcf4077d3dc5e66edf688040cde2e33e
Thanh Vu <csnsjh@gmail.com>
2021-08-25 13:18:16 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,7 +71,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    vendor: ['vue-apexchart'],
+    // vendor: ['vue-apexchart'],
   },
 
   googleAnalytics: {
