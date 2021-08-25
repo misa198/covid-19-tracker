51a1e717bd1b56f86f9b55a381c0d06ab190f8b2
Thanh Vu <csnsjh@gmail.com>
2021-08-25 09:39:16 +0700

    vaccine store

--- services/app.service.ts
+++ services/app.service.ts
@@ -15,3 +15,30 @@ export const fetchAppVietNamSummary =
   async (): Promise<AppVietnamSummaryResponse> => {
     return await axiosAppClient.get('/vietnam');
   };
+
+export interface AppVietnamVaccineStatistic {
+  dayInjection: {
+    date: string;
+    first: number;
+    second: number;
+    average: number;
+  };
+  summaryInjection: {
+    date: string;
+    first: number;
+    second: number;
+  };
+  firstTotal: number;
+  firstRatio: number;
+  secondTotal: number;
+  secondRatio: number;
+}
+
+export interface AppVietnamVaccineStatisticResponse {
+  data: AppVietnamVaccineStatistic;
+}
+
+export const fetchAppVietNamVaccineStatistic =
+  async (): Promise<AppVietnamVaccineStatisticResponse> => {
+    return await axiosAppClient.get('/vaccine');
+  };
---
+++ store/vaccine.ts
@@ -0,0 +1,72 @@
+import Vue from 'vue';
+import { ActionContext } from 'vuex/types';
+import { RootState } from '.';
+import {
+  AppVietnamVaccineStatistic,
+  AppVietnamVaccineStatisticResponse,
+  fetchAppVietNamVaccineStatistic,
+} from '~/services/app.service';
+
+interface VaccineState {
+  statistic: {
+    data: AppVietnamVaccineStatistic;
+    loading: boolean;
+    error: boolean;
+  };
+}
+
+export const state: () => VaccineState = () => ({
+  statistic: {
+    data: {
+      dayInjection: {
+        date: '',
+        first: 0,
+        second: 0,
+        average: 0,
+      },
+      summaryInjection: {
+        date: '',
+        first: 0,
+        second: 0,
+      },
+      firstTotal: 0,
+      firstRatio: 0,
+      secondTotal: 0,
+      secondRatio: 0,
+    },
+    loading: false,
+    error: false,
+  },
+});
+
+export const mutations = {
+  fetchVaccineDataPending(state: VaccineState) {
+    Vue.set(state.statistic, 'loading', true);
+    Vue.set(state.statistic, 'error', false);
+  },
+  fetchVaccineDataFulfilled(
+    state: VaccineState,
+    payload: AppVietnamVaccineStatisticResponse
+  ) {
+    Vue.set(state.statistic, 'loading', false);
+    Vue.set(state.statistic, 'data', payload);
+  },
+  fetchVaccineDataRejected(state: VaccineState) {
+    Vue.set(state.statistic, 'loading', false);
+    Vue.set(state.statistic, 'error', true);
+  },
+};
+
+export const actions = {
+  async fetchAppVietNamVaccineStatistic({
+    commit,
+  }: ActionContext<VaccineState, RootState>) {
+    commit('fetchVaccineDataPending');
+    try {
+      const res = await fetchAppVietNamVaccineStatistic();
+      commit('fetchVaccineDataFulfilled', res);
+    } catch (e) {
+      commit('fetchVaccineDataRejected');
+    }
+  },
+};
