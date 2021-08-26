1ffc0ffac41dda9f0c9f11466f33888176c90a58
Thanh Vu <csnsjh@gmail.com>
2021-08-26 15:31:31 +0700

    update

--- api/index.ts
+++ api/index.ts
@@ -7,21 +7,18 @@ import log from 'fancy-log';
 import vietnamRoute from './routers/vietnam.route';
 import vaccineRoute from './routers/vaccine.route';
 
-const isDev = process.env.NODE_ENV !== 'production';
-const port = process.env.PORT || 5000;
-
 const app = express();
 
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use('/vietnam', vietnamRoute);
 app.use('/vaccine', vaccineRoute);
 
-if (!isDev) {
-  app.use(
-    helmet({
-      contentSecurityPolicy: false,
-    })
-  );
-} else {
+if (require.main === module) {
+  const port = process.env.PORT || 5000;
   app.listen(port, () => {
     log('Server ready on port ' + port);
   });
