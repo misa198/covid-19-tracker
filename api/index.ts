3d84f13b516d171ce8d6937242d9d5fb9912c9ca
Thanh Vu <csnsjh@gmail.com>
2021-08-26 15:24:49 +0700

    update api middleware

--- api/index.ts
+++ api/index.ts
@@ -7,32 +7,24 @@ import log from 'fancy-log';
 import vietnamRoute from './routers/vietnam.route';
 import vaccineRoute from './routers/vaccine.route';
 
-const { loadNuxt, build } = require('nuxt');
 const isDev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 5000;
 
-async function start() {
-  const app = express();
+const app = express();
 
-  app.use('/api/vietnam', vietnamRoute);
-  app.use('/api/vaccine', vaccineRoute);
+app.use('/vietnam', vietnamRoute);
+app.use('/vaccine', vaccineRoute);
 
-  const nuxt = await loadNuxt(isDev ? 'dev' : 'start');
-  if (isDev) {
-    build(nuxt);
-  } else {
-    app.use(
-      helmet({
-        contentSecurityPolicy: false,
-      })
-    );
-  }
-
-  app.use(nuxt.render);
-
+if (!isDev) {
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    })
+  );
+} else {
   app.listen(port, () => {
     log('Server ready on port ' + port);
   });
 }
 
-start();
+export default app;
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   target: 'server',
   serverMiddleware: {
-    '/api': '~/server',
+    '/api': '~/api',
   },
 
   head: {
