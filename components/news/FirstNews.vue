3dff68a8d357fa11c5a64237dba44422a27c900c
Thanh Vu <csnsjh@gmail.com>
2021-09-02 01:25:00 +0700

    fix onerror url image

--- components/news/FirstNews.vue
+++ components/news/FirstNews.vue
@@ -27,8 +27,10 @@
               width: '100%',
               height: '100%',
               position: 'absolute',
+              top: 0,
+              left: 0,
             }"
-            :onerror="defaultNewsCoverImage"
+            :onerror="`this.src='${defaultNewsCoverImage}'`"
           />
         </div>
       </v-row>
